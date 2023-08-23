ba1fd9511ee54c62be6008d572a984fe88e2ab00
Artem Badrak <artem12895@icloud.com>
2023-08-24 01:27:46 +0300

    added error boundary, added 404 error ui

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { ErrorBoundary } from 'react-error-boundary'
 
 import Layout from "../Layout";
 import HomePage from "../pages/HomePage";
 import CategoryPage from "../pages/CategoryPage";
+import FallbackComponent from "../FallbackComponent";
+import NotFoundPage from "../NotFoundPage";
 import { useSelector } from "react-redux";
 import { userRegisterStateSelector } from "../DrawerContent/selector";
+import { logErrorToService } from '../../utils/utils'
 
 const App: React.FC = () => {
   const { isAuth } = useSelector(userRegisterStateSelector);
@@ -16,16 +20,24 @@ const App: React.FC = () => {
     );
   }, [isAuth]);
   return (
-    <>
+    <ErrorBoundary
+      FallbackComponent={FallbackComponent}
+      onReset={() => {
+        // reset the state
+      }}
+      resetKeys={['someKey']}
+      onError={logErrorToService}
+    >
       <Router>
         <Layout>
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/:categoryName" element={<CategoryPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </Layout>
       </Router>
-    </>
+    </ErrorBoundary>
   );
 };
 
---
+++ src/utils/utils.ts
@@ -0,0 +1,3 @@
+export function logErrorToService(error: Error, info: React.ErrorInfo) {
+    console.error("Error reporting service:", error, info);
+}
