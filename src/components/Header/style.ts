c9802e83f2ff789c84974ca5a794b9d85fc4f713
Artem Badrak <artem12895@icloud.com>
2023-10-29 19:29:39 +0200

    remove overflow for header and footer

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -5,65 +5,64 @@ import {
   styled,
   IconButton,
   Box,
-} from "@mui/material";
-import { NavLink } from "react-router-dom";
+} from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
 export const StyledAppBar = styled(AppBar)(({ theme }) => ({
-  boxShadow: "none",
-  justifyContent: "center",
+  boxShadow: 'none',
+  justifyContent: 'center',
   padding: theme.spacing(2, 0),
-  backgroundColor: "transparent",
+  backgroundColor: 'transparent',
 }));
 
 export const StyledToolBar = styled(Toolbar)(({ theme }) => ({
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
   borderRadius: 16,
   padding: theme.spacing(2, 3),
   backgroundColor: theme.palette.background.paper,
-  overflow: 'hidden',
 }));
-export const StyledLogo = styled("img")(({ theme }) => ({
-  width: "100%",
+export const StyledLogo = styled('img')(({ theme }) => ({
+  width: '100%',
 }));
 
 export const StyledButton = styled(Button)(({ theme }) => ({
   padding: theme.spacing(1, 2),
-  [theme.breakpoints.down("md")]: {
-    fontSize: "1rem",
+  [theme.breakpoints.down('md')]: {
+    fontSize: '1rem',
   },
-  ":hover": {
+  ':hover': {
     backgroundColor: theme.palette.primary.light,
   },
 }));
 
 export const StyledIconButton = styled(IconButton)(({ theme }) => ({
-  border: "1px solid",
+  border: '1px solid',
   padding: theme.spacing(1),
   borderColor: theme.palette.secondary.light,
 }));
 
 export const CustomDivider = styled(Box)(({ theme }) => ({
-  border: "1px solid",
+  border: '1px solid',
   borderColor: theme.palette.divider,
   height: theme.spacing(2.5),
 }));
 
 export const StyledLink = styled(NavLink)(({ theme }) => ({
-  width: "55%",
-  display: "flex",
-  "&.active": {
-    pointerEvents: "none",
+  width: '55%',
+  display: 'flex',
+  '&.active': {
+    pointerEvents: 'none',
   },
 }));
 
 export const StyledTextButton = styled(Button)(({ theme }) => ({
-  fontSize: "1rem",
+  fontSize: '1rem',
   color: theme.palette.text.primary,
   fontWeight: 400,
-  ":hover": {
+  ':hover': {
     color: theme.palette.primary.main,
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
   },
 }));
