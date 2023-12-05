4fbe39fa5ca3b5776a09a6b2c7340d84823db1b0
Artem-H5 <95889999+Artem-H5@users.noreply.github.com>
2023-12-05 22:43:08 +0200

    added seller rating adaptive (#82)

--- src/components/pages/CategoryPage/Filters/RatingFilter.tsx
+++ src/components/pages/CategoryPage/Filters/RatingFilter.tsx
@@ -13,9 +13,12 @@ import {
   ListItemButton,
   Collapse,
   Stack,
+  Box,
 } from '@mui/material';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import {
   FilterText,
   GoodtypeFormControlLabel,
@@ -32,6 +35,8 @@ const RatingFilter: React.FC<RatingFilterProps> = ({
   setRatingValue,
 }) => {
   const dispatch: AppDispatch = useDispatch();
+  const theme = useTheme();
+  const isMdScreen = useMediaQuery(theme.breakpoints.down('md'));
 
   const { filterBy } = useSelector(productsStateSelector);
   const [ratingOpen, setRatingOpen] = React.useState(true);
@@ -83,46 +88,83 @@ const RatingFilter: React.FC<RatingFilterProps> = ({
         '& .MuiRating-root': { gap: '.5rem' },
       }}
     >
-      <FormLabel>
-        <ListItemButton onClick={ratingHandleClick} sx={{ mb: '.5rem' }}>
-          <FilterText primary='За рейтингом продавця' />
-          {ratingOpen ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
-      </FormLabel>
-      <Collapse
-        in={ratingOpen}
-        timeout='auto'
-        unmountOnExit
-        onChange={handleRatingChange}
-      >
-        <FormGroup
-          sx={{
-            mb: '1rem',
-          }}
-          aria-labelledby='radio-buttons-group-label'
-        >
-          {Array.from({ length: 4 }, (_, index) => {
-            const rating = 5 - index;
-            return (
-              <FormControlLabel
-                key={rating}
-                value={rating}
-                control={
-                  <Checkbox checked={ratingValue.includes(String(rating))} />
-                }
-                label={
-                  <Rating
-                    name='read-only'
+      {!isMdScreen ? (
+        <>
+          <FormLabel>
+            <ListItemButton onClick={ratingHandleClick} sx={{ mb: '.5rem' }}>
+              <FilterText primary='За рейтингом продавця' />
+              {ratingOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+          </FormLabel>
+          <Collapse
+            in={ratingOpen}
+            timeout='auto'
+            unmountOnExit
+            onChange={handleRatingChange}
+          >
+            <FormGroup
+              sx={{
+                mb: '1rem',
+              }}
+              aria-labelledby='radio-buttons-group-label'
+            >
+              {Array.from({ length: 4 }, (_, index) => {
+                const rating = 5 - index;
+                return (
+                  <FormControlLabel
+                    key={rating}
                     value={rating}
-                    size='small'
-                    readOnly
+                    control={
+                      <Checkbox
+                        checked={ratingValue.includes(String(rating))}
+                      />
+                    }
+                    label={
+                      <Rating
+                        name='read-only'
+                        value={rating}
+                        size='small'
+                        readOnly
+                      />
+                    }
                   />
-                }
-              />
-            );
-          })}
-        </FormGroup>
-      </Collapse>
+                );
+              })}
+            </FormGroup>
+          </Collapse>
+        </>
+      ) : (
+        <Box onChange={handleRatingChange}>
+          <FilterText primary='За рейтингом продавця' sx={{ mb: '.5rem' }} />
+          <FormGroup
+            sx={{
+              mb: '1rem',
+            }}
+            aria-labelledby='radio-buttons-group-label'
+          >
+            {Array.from({ length: 4 }, (_, index) => {
+              const rating = 5 - index;
+              return (
+                <FormControlLabel
+                  key={rating}
+                  value={rating}
+                  control={
+                    <Checkbox checked={ratingValue.includes(String(rating))} />
+                  }
+                  label={
+                    <Rating
+                      name='read-only'
+                      value={rating}
+                      size='small'
+                      readOnly
+                    />
+                  }
+                />
+              );
+            })}
+          </FormGroup>
+        </Box>
+      )}
     </Stack>
   );
 };
