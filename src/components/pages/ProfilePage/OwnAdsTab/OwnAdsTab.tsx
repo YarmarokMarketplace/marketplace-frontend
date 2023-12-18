2e71c5c6e6edbc2aff31b29176b336700a852005
Artem-H5 <95889999+Artem-H5@users.noreply.github.com>
2023-12-18 20:58:58 +0200

    added ad views (#85)

--- src/components/pages/ProfilePage/OwnAdsTab/OwnAdsTab.tsx
+++ src/components/pages/ProfilePage/OwnAdsTab/OwnAdsTab.tsx
@@ -6,6 +6,8 @@ import OwnProductItem from '../ProductItem/OwnProductItem';
 import { StyledContrastButton, StyledIconButton } from '../ProductItem/style';
 
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import PhoneIcon from '@mui/icons-material/Phone';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 import NoProductItem from './NoProductItem';
 import { useDispatch, useSelector } from 'react-redux';
 import { ownAdsStateSelector } from '../../../../redux/profile/selector';
@@ -39,7 +41,7 @@ const CustomTabPanel: React.FC<TabPanelProps> = (props) => {
 
   return (
     <div
-      role="tabpanel"
+      role='tabpanel'
       hidden={value !== type}
       id={`own-ads-tab-${type}`}
       aria-labelledby={`own-ads-tab-${type}`}
@@ -119,7 +121,7 @@ const OwnAdsTab = () => {
   return (
     <StyledAdsContainer>
       <StyledTitleContainer>
-        <Typography variant="h4">Оголошення</Typography>
+        <Typography variant='h4'>Оголошення</Typography>
         <Tabs
           value={value}
           onChange={handleChange}
@@ -136,54 +138,69 @@ const OwnAdsTab = () => {
           }}
         >
           <StyledTab
-            label="Активні"
-            id="own-ads-tab-0"
-            aria-controls="own-ads-tabpanel-0"
-            value="active"
+            label='Активні'
+            id='own-ads-tab-0'
+            aria-controls='own-ads-tabpanel-0'
+            value='active'
           />
           <StyledTab
-            label="Неактивні"
-            id="own-ads-tab-1"
-            aria-controls="own-ads-tabpanel-1"
-            value="inactive"
+            label='Неактивні'
+            id='own-ads-tab-1'
+            aria-controls='own-ads-tabpanel-1'
+            value='inactive'
           />
         </Tabs>
       </StyledTitleContainer>
-      <CustomTabPanel value={value} type="active">
+      <CustomTabPanel value={value} type='active'>
         {loading && <SkeletonAds limit={limit} />}
         {!loading && !error && activeNotices.length > 0 && (
           <Stack gap={3}>
             {activeNotices.map((product) => {
+              console.log(product._id);
               return (
                 <OwnProductItem product={product} key={product._id}>
-                  <Stack justifyContent="space-between" alignItems="flex-end">
+                  <Stack justifyContent='space-between' alignItems='flex-end'>
                     <Stack
-                      direction="row"
+                      direction='row'
                       gap={3}
-                      justifyContent="space-between"
-                      height="fit-content"
+                      justifyContent='space-between'
+                      height='fit-content'
                     >
                       <StyledContrastButton
                         data-product-id={product._id}
-                        id="deactivate-btn"
-                        variant="outlined"
+                        id='deactivate-btn'
+                        variant='outlined'
                         onClick={handleDeactivateClick}
                       >
                         Деактивувати
                       </StyledContrastButton>
-                      <StyledIconButton id="edit-btn">
+                      <StyledIconButton id='edit-btn'>
                         <EditOutlinedIcon
                           sx={{ fontSize: '1.5rem' }}
-                          color="primary"
+                          color='primary'
                           onClick={() =>
                             navigate(`/edit-advert/${product._id}`)
                           }
                         />
                       </StyledIconButton>
                     </Stack>
-                    <Typography variant="body1" color="secondary.dark">
-                      {<Moment format="DD/MM/YY">{product.createdAt}</Moment>}
-                    </Typography>
+                    <Stack direction='row' spacing={2}>
+                      <Stack direction='row' spacing={1}>
+                        <VisibilityIcon fontSize='small' color='secondary' />
+                        <Typography variant='body1' color='secondary.dark'>
+                          {product.views}
+                        </Typography>
+                      </Stack>
+                      <Stack direction='row' spacing={1}>
+                        <PhoneIcon fontSize='small' color='secondary' />
+                        <Typography variant='body1' color='secondary.dark'>
+                          {product.contactsViews}
+                        </Typography>
+                      </Stack>
+                      <Typography variant='body1' color='secondary.dark'>
+                        {<Moment format='DD/MM/YY'>{product.createdAt}</Moment>}
+                      </Typography>
+                    </Stack>
                   </Stack>
                 </OwnProductItem>
               );
@@ -193,14 +210,14 @@ const OwnAdsTab = () => {
         {!loading && activeNotices.length === 0 && (
           <>
             <NoProductItem>
-              <Typography variant="h4" fontWeight={700} mt={3}>
+              <Typography variant='h4' fontWeight={700} mt={3}>
                 Активні оголошення відображаються тут до закінчення їх терміну
                 дії
               </Typography>
               <Typography
-                variant="body1"
+                variant='body1'
                 fontWeight={500}
-                color="text.secondary"
+                color='text.secondary'
                 mt={1}
               >
                 Ці оголошення доступні для перегляду всім і стають неактивними
@@ -217,7 +234,7 @@ const OwnAdsTab = () => {
           />
         )}
       </CustomTabPanel>
-      <CustomTabPanel value={value} type="inactive">
+      <CustomTabPanel value={value} type='inactive'>
         {loading && <SkeletonAds limit={limit} />}
         {!loading && !error && inactiveNotices.length > 0 && (
           <Stack gap={3}>
@@ -228,33 +245,33 @@ const OwnAdsTab = () => {
                     <Button
                       data-product-id={product._id}
                       onClick={handleActivateProductClick}
-                      id="activate-btn"
-                      variant="outlined"
+                      id='activate-btn'
+                      variant='outlined'
                       fullWidth
                     >
                       Активувати
                     </Button>
                     <Stack
-                      direction="row"
+                      direction='row'
                       gap={3}
-                      justifyContent="center"
-                      height="fit-content"
+                      justifyContent='center'
+                      height='fit-content'
                     >
                       <StyledContrastButton
                         data-del-btn-id={product._id}
-                        id="delete-btn"
-                        variant="outlined"
+                        id='delete-btn'
+                        variant='outlined'
                         onClick={handleClickDeleteProduct}
                       >
                         Видалити
                       </StyledContrastButton>
                       <StyledIconButton
-                        id="edit-btn"
+                        id='edit-btn'
                         onClick={() => navigate(`/edit-advert/${product._id}`)}
                       >
                         <EditOutlinedIcon
                           sx={{ fontSize: '1.5rem' }}
-                          color="primary"
+                          color='primary'
                         />
                       </StyledIconButton>
                     </Stack>
@@ -267,13 +284,13 @@ const OwnAdsTab = () => {
         {!loading && inactiveNotices.length === 0 && (
           <>
             <NoProductItem>
-              <Typography variant="h4" fontWeight={700} mt={3}>
+              <Typography variant='h4' fontWeight={700} mt={3}>
                 Оголошення переміщуються сюди після закінчення терміну дії
               </Typography>
               <Typography
-                variant="body1"
+                variant='body1'
                 fontWeight={500}
-                color="text.secondary"
+                color='text.secondary'
                 mt={1}
               >
                 Ви також можете деактивувати оголошення до закінчення терміну
